b705e15ea99d22621073b442a347cc1fa8cb7ee9
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-03-25 19:56:36 +0300

    Add files via upload

--- Vavoo/src/main/kotlin/com/nikyokki/Vavoo.kt
+++ Vavoo/src/main/kotlin/com/nikyokki/Vavoo.kt
@@ -2,19 +2,7 @@ package com.nikyokki
 
 import android.util.Log
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.cloudstream3.HomePageList
-import com.lagradost.cloudstream3.HomePageResponse
-import com.lagradost.cloudstream3.LiveSearchResponse
-import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.VPNStatus
-import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.newHomePageResponse
-import com.lagradost.cloudstream3.newLiveStreamLoadResponse
+import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
@@ -22,7 +10,6 @@ import com.lagradost.cloudstream3.utils.Qualities
 
 class Vavoo() : MainAPI() {
     override var mainUrl = "https://vavoo.to"
-    val proxyUrl = "https://wproxy.net"
     override var name = "Vavoo"
     override var lang = "tr"
     override val supportedTypes = setOf(TvType.Live)
@@ -31,47 +18,32 @@ class Vavoo() : MainAPI() {
     override val vpnStatus = VPNStatus.MightBeNeeded
 
     private suspend fun getChannels(): List<Channel> {
-        val mainReq = app.get(proxyUrl)
-        cookie = mainReq.cookies["PHPSESSID"].toString()
-        val document = app.get("https://sv1.wproxy.info/proxy.php/$mainUrl/channels", headers = headers,
-            cookies = mapOf(
-                "PHPSESSID" to cookie
-            )).body.string()
+        val document = app.get("$mainUrl/channels", headers = headers).body.string()
         return parseJson<List<Channel>>(document)
     }
 
     companion object {
         var channels = emptyList<Channel>()
-        var cookie = ""
-        val headers = mapOf("Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        val headers = mapOf(
+            "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Language" to "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Connection" to "keep-alive", "Referer" to "https://wproxy.net",
-            "Sec-Fetch-Dest" to "document", "Sec-Fetch-Mode" to "navigate", "Sec-Fetch-Site" to "same-origin", "Sec-Fetch-User" to "?1",
-            "Upgrade-Insecure-Requests" to "1", "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
-            "sec-ch-ua" to "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"", "sec-ch-ua-mobile" to "?0",
-            "sec-ch-ua-platform" to "\"Windows\"")
+            "Connection" to "keep-alive",
+            "Referer" to "https://vavoo.to",
+            "User-Agent" to "VAVOO/1.0"
+        )
 
-        @Suppress("ConstPropertyName")
-        const val posterLink =
-            "https://raw.githubusercontent.com/doGior/doGiorsHadEnough/master/Huhu/tv.png"
+        const val posterLink = "https://raw.githubusercontent.com/doGior/doGiorsHadEnough/master/Huhu/tv.png"
     }
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         if (channels.isEmpty()) {
             channels = getChannels()
         }
-        val sections =
-            channels.groupBy { it.country }.map {
-                HomePageList(
-                    it.key,
-                    it.value.map { channel -> channel.toSearchResponse(this.name) },
-                    false
-                )
-            }.sortedBy { it.name }
+        val sections = channels.groupBy { it.country }.map {
+            HomePageList(it.key, it.value.map { channel -> channel.toSearchResponse(this.name) }, false)
+        }.sortedBy { it.name }
 
-        return newHomePageResponse(
-            sections, false
-        )
+        return newHomePageResponse(sections, false)
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
@@ -79,8 +51,7 @@ class Vavoo() : MainAPI() {
             channels = getChannels()
         }
         val matches = channels.filter { channel ->
-            query.lowercase().replace(" ", "") in
-                    channel.name.lowercase().replace(" ", "")
+            query.lowercase().replace(" ", "") in channel.name.lowercase().replace(" ", "")
         }
         return matches.map { it.toSearchResponse(this.name) }
     }
@@ -91,7 +62,6 @@ class Vavoo() : MainAPI() {
         return newLiveStreamLoadResponse(channel.name, "$mainUrl/play/${channel.id}/index.m3u8", "$mainUrl/play/${channel.id}/index.m3u8") {
             this.posterUrl = posterLink
             this.tags = listOf(channel.country)
-            this.recommendations = recommendations
         }
     }
 
@@ -102,17 +72,13 @@ class Vavoo() : MainAPI() {
         callback: (ExtractorLink) -> Unit,
     ): Boolean {
         Log.d("TV2", data)
-        var url = data
-        if (data.startsWith("https://vavoo.to")) {
-            url = "https://sv1.wproxy.info/proxy.php/$data"
-        }
         callback(
             ExtractorLink(
                 this.name,
                 this.name,
-                url,
+                data,
                 headers = headers,
-                referer = proxyUrl,
+                referer = mainUrl,
                 quality = Qualities.Unknown.value,
                 isM3u8 = true
             )
