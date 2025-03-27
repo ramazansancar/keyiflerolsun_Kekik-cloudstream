e5d443e81bf00fc059a00e5f36bd87dc77080b61
feroxx <frhatfrat@gmail.com>
2025-03-27 11:40:17 +0300

    a

--- KultFilmler/src/main/kotlin/com/keyiflerolsun/KultFilmler.kt
+++ KultFilmler/src/main/kotlin/com/keyiflerolsun/KultFilmler.kt
@@ -77,7 +77,7 @@ class KultFilmler : MainAPI() {
         val document = app.get(request.data).document
         Log.d("getMainPage", "Sayfa yüklendi: ${request.data}")
 
-        val movieBoxes = document.select("div.row div.film-box div.movie-box")
+        val movieBoxes = document.select("div.col-lg-8 div.row div.film-box div.movie-box")
         Log.d("getMainPage", "Bulunan movie-box sayısı: ${movieBoxes.size}")
 
         val home = movieBoxes.mapNotNull { 
