bae051f1f64171d8209679e61357f32d0a91a4a0
feroxx <frhatfrat@gmail.com>
2025-03-27 15:11:32 +0300

    sd

--- KultFilmler/src/main/kotlin/com/keyiflerolsun/KultFilmler.kt
+++ KultFilmler/src/main/kotlin/com/keyiflerolsun/KultFilmler.kt
@@ -89,7 +89,7 @@ class KultFilmler : MainAPI() {
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("a")?.attr("title")?.let { fixUrlNull(it) } ?: return null
+        val title = this.selectFirst("a")?.let { a ->a.attr("title").ifEmpty { a.text() }} ?: return null
         Log.d("toSearchResult", "Title: $title")
 
         val href = this.selectFirst("a")?.attr("href")?.let { fixUrlNull(it) } ?: return null
