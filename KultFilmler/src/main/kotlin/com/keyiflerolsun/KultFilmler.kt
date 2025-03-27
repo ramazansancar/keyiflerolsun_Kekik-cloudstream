60bcf1297b4754933512249294f559d39fc065c1
feroxx <frhatfrat@gmail.com>
2025-03-27 03:07:02 +0300

    kf

--- KultFilmler/src/main/kotlin/com/keyiflerolsun/KultFilmler.kt
+++ KultFilmler/src/main/kotlin/com/keyiflerolsun/KultFilmler.kt
@@ -7,6 +7,11 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
+import com.lagradost.cloudstream3.network.CloudflareKiller
+import com.lagradost.cloudstream3.utils.StringUtils.decodeUri
+import okhttp3.Interceptor
+import okhttp3.Response
 import android.util.Base64
 import org.jsoup.Jsoup
 
