45b00d160ba29e522e99b0e39f2afc6158d07f60
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-03-30 21:08:21 +0300

    Update Watch32Provider.kt

--- Watch32/src/main/kotlin/com/kerimmkirac/Watch32Provider.kt
+++ Watch32/src/main/kotlin/com/kerimmkirac/Watch32Provider.kt
@@ -40,8 +40,8 @@ class Watch32Provider : MainAPI() {
         "movie" to "Popular Movies",
         "tv-show" to "Popular TV Shows",
         "genre/animation" to "Animations",
-        "country/IN" to "India",
-        "country/FR" to "France"
+        "country/TR" to "Türkiye",
+        "country/US" to "America"
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest
