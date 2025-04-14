bf0447414ccf74dd7f2382f153fef9ef34027c83
feroxx <frhatfrat@gmail.com>
2025-04-14 09:45:05 +0300

    jtf

--- JetFilmizle/src/main/kotlin/com/keyiflerolsun/JetFilmizlePlugin.kt
+++ JetFilmizle/src/main/kotlin/com/keyiflerolsun/JetFilmizlePlugin.kt
@@ -8,5 +8,8 @@ import android.content.Context
 class JetFilmizlePlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(JetFilmizle())
+        registerExtractorAPI(Odnoklassniki())
+        registerExtractorAPI(OkRuSSL())
+        registerExtractorAPI(OkRuHTTP())
     }
 }
