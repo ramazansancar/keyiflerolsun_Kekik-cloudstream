48da5f61d98c79a3a3b2c55bd4eee18ec9f3a10d
feroxx <frhatfrat@gmail.com>
2025-04-15 16:39:40 +0300

    wbti

--- WebteIzle/src/main/kotlin/com/keyiflerolsun/DzenRu.kt
+++ WebteIzle/src/main/kotlin/com/keyiflerolsun/DzenRu.kt
@@ -1,49 +1,45 @@
+
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
 package com.keyiflerolsun
 
-import android.util.Log
+import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.fasterxml.jackson.annotation.JsonProperty
 
-open class MailRu : ExtractorApi() {
+open class DzenRu : ExtractorApi() {
     override val name            = "DzenRu"
     override val mainUrl         = "https://dzen.ru"
-    override val requiresReferer = false
+    override val requiresReferer = true
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        Log.d("Kekik_${this.name}", "url » $url")
+        val extRef   = referer ?: ""
+        val videoKey = url.split("/").last()
+        val videoUrl = "${mainUrl}/embed/${videoKey}"
 
-        val vidId     = url.substringAfter("embed/").trim()
-        Log.d("Kekik_${this.name}", "vidId » $vidId")
-		
-		val videoData = AppUtils.tryParseJson<DzenRuData>(vidId.text) ?: throw ErrorLoadingException("Video not found")
+        val api = app.get(videoUrl).parsedSafe<DzenRuUrls>() ?: throw ErrorLoadingException("DzenRu")
 
-        for (video in videoData.videos) {
-            Log.d("Kekik_${this.name}", "video » $video")
-
-            val videoUrl = if (video.url.startsWith("//")) "https:${video.url}" else video.url
-
+        for (video in api.urls) {
             callback.invoke(
                 newExtractorLink(
                     source  = this.name,
                     name    = this.name,
-                    url     = videoUrl,
-			) {
-                this.headers = mapOf("Referer" to "${mainUrl}/")
-                this.quality = Qualities.Unknown.value
-            }
+                    url     = video.url,
+                ) {
+                    this.referer = extRef
+                    this.quality = Qualities.Unknown.value
+                }
             )
         }
     }
 
-    data class DzenRuData(
-        @JsonProperty("provider") val provider: String,
-        @JsonProperty("videos")   val videos: List<DzenRuVideoData>
+    data class DzenRuUrls(
+        @JsonProperty("urls") val urls: List<DzenRuData>
     )
 
-    data class DzenRuVideoData(
-        @JsonProperty("url") val url: String,
+    data class DzenRuData(
+        @JsonProperty("url")   val url: String,
+        @JsonProperty("label") val label: String,
     )
 }
