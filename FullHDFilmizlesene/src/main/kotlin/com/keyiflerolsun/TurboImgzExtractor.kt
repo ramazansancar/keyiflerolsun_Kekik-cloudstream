894afda0dd2c19f29638fc3eab2c51512e3f0df2
feroxx <frhatfrat@gmail.com>
2025-04-18 23:25:25 +0300

    Update TurboImgzExtractor.kt

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TurboImgzExtractor.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TurboImgzExtractor.kt
@@ -13,7 +13,7 @@ open class TurboImgz : ExtractorApi() {
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
         val extRef   = referer ?: ""
-        val videoReq = app.get(url.split("/").last(), referer=extRef).text
+        val videoReq = app.get(url.split("/").last()).text
 		Log.d("Kekik_${this.name}", "videoReq » $videoReq")
 
         val videoLink = Regex("""file: "(.*)",""").find(videoReq)?.groupValues?.get(1) ?: throw ErrorLoadingException("File not found")
