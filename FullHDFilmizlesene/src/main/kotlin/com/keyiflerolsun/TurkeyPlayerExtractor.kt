d9cc23b91b59ba92f0839b286beb853cfc44b4f4
feroxx <frhatfrat@gmail.com>
2025-04-19 16:31:38 +0300

    fhd

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TurkeyPlayerExtractor.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TurkeyPlayerExtractor.kt
@@ -12,12 +12,21 @@ open class TurkeyPlayer : ExtractorApi() {
     override val requiresReferer = true
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        val extRef   = referer ?: ""
-        val videoReq = app.get(url.split("/").last(), referer=extRef).text
-		Log.d("Kekik_${this.name}", "videoReq » $videoReq")
+        val extRef = referer ?: ""
+        val pageContent = app.get(url, referer = extRef).text
+        Log.d("Kekik_${this.name}", "PageContent » $pageContent")
 
-        val videoLink = Regex("""file: "(.*)",""").find(videoReq)?.groupValues?.get(1) ?: throw ErrorLoadingException("File not found")
-        Log.d("Kekik_${this.name}", "videoLink » $videoLink")
+        // ID'yi al
+        val id = Regex("""id=(\d+)""").find(url)?.groupValues?.get(1)
+            ?: Regex("""data-id=['"]?(\d+)['"]?""").find(pageContent)?.groupValues?.get(1)
+            ?: throw ErrorLoadingException("Video ID bulunamadı")
+
+        // md5 hash'i çek
+        val hash = Regex("""/m3u8/8/([a-fA-F0-9]{32})/master\.txt""").find(pageContent)?.groupValues?.get(1)
+            ?: throw ErrorLoadingException("MD5 hash bulunamadı")
+
+        val masterUrl = "https://watch.turkeyplayer.com/m3u8/8/$hash/master.txt?s=1&id=$id&cache=1"
+        Log.d("Kekik_${this.name}", "masterUrl » $masterUrl")
 
         callback.invoke(
             newExtractorLink(
