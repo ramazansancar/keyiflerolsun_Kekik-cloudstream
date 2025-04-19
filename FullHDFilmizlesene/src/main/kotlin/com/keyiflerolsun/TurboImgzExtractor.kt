ccc0400f623cd6e518743278815c8b095ee47ae0
feroxx <frhatfrat@gmail.com>
2025-04-19 15:31:49 +0300

    Update TurboImgzExtractor.kt

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TurboImgzExtractor.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TurboImgzExtractor.kt
@@ -8,13 +8,12 @@ import com.lagradost.cloudstream3.utils.*
 
 open class TurboImgz : ExtractorApi() {
     override val name            = "TurboImgz"
-    override val mainUrl         = "https://watch.turkeyplayer.com/"
+    override val mainUrl         = "https://turbo.imgz.me"
     override val requiresReferer = true
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
         val extRef   = referer ?: ""
-        val videoReq = app.get(url.split("/").last()).text
-		Log.d("Kekik_${this.name}", "videoReq » $videoReq")
+        val videoReq = app.get(url.substringAfter("||"), referer=extRef).text
 
         val videoLink = Regex("""file: "(.*)",""").find(videoReq)?.groupValues?.get(1) ?: throw ErrorLoadingException("File not found")
         Log.d("Kekik_${this.name}", "videoLink » $videoLink")
