f00d278649a68607464f5dfb005e99f612995e14
feroxx <frhatfrat@gmail.com>
2025-04-27 13:21:07 +0300

    stf

--- SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetPlayExtractor.kt
+++ SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetPlayExtractor.kt
@@ -28,7 +28,7 @@ open class SetPlay : ExtractorApi() {
                 source  = this.name,
                 name    = "${this.name} - $title",
                 url     = m3uLink,
-                type = ExtractorLinkType.DASH
+                type = ExtractorLinkType.M3U8
             ) {
                 quality = Qualities.Unknown.value
                 headers = mapOf("Referer" to url)
