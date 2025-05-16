c1a678b1abc8112f2e06b9132511210ad97d3d5a
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-05-16 17:32:32 +0300

    Sinefy

---
+++ Sinefy/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("kerimmkirac")
+    language    = "tr"
+    description = "Yabancı film izle olarak vizyondaki en yeni yabancı filmleri türkçe dublaj ve altyazılı olarak en hızlı şekilde full hd olarak sizlere sunuyoruz."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie, TvSeries")
+    iconUrl = "https://sinefy3.com/assets/img/sinefy/icon-144x144.png"
+}
