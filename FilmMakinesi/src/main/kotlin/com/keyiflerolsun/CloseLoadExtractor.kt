5dd602c033e7ad3d0881f086a4ab9213ef45d846
feroxx <frhatfrat@gmail.com>
2025-05-18 23:32:13 +0300

    Update CloseLoadExtractor.kt

--- FilmMakinesi/src/main/kotlin/com/keyiflerolsun/CloseLoadExtractor.kt
+++ FilmMakinesi/src/main/kotlin/com/keyiflerolsun/CloseLoadExtractor.kt
@@ -34,7 +34,7 @@ val iSource = app.get(url, referer = extRef)
         else -> "$mainUrl/$src"
     }
     val label = it.attr("label") ?: "Altyazı"
-            Log.d("Kekik_${this.name}", "fullUrl » $fullUrl)
+            Log.d("Kekik_${this.name}", "fullUrl » $full Url")
 
     // Altyazıyı callback ile bildiriyoruz
     subtitleCallback(
