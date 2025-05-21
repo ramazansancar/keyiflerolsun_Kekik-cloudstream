c80ce7215225b0225201951f43de6963733b7077
feroxx <frhatfrat@gmail.com>
2025-05-21 23:57:25 +0300

    Update DiziPal.kt

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -19,8 +19,8 @@ class DiziPal : MainAPI() {
 
     // ! CloudFlare bypass
     override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
-    // override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
-    // override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
+    override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
+    override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
 
     override val mainPage = mainPageOf(
         "${mainUrl}/diziler/son-bolumler"                          to "Son Bölümler",
