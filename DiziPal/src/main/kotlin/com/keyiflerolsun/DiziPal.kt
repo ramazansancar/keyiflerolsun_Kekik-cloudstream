c16378346310cc69b5bcd7f7c6429919030df9a2
feroxx <frhatfrat@gmail.com>
2025-05-22 10:33:29 +0300

    dzp

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -250,9 +250,8 @@ class DiziPal : MainAPI() {
         private const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
 
         private fun getHeaders(referer: String): Map<String, String> = mapOf(
-            "accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-            "user-agent" to USER_AGENT,
-            "referer" to referer
+            "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+            "User-Agent" to USER_AGENT,
         )
         }
 }
