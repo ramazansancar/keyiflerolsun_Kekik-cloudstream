77af1477d9d6bbe8b205d51ddab2110bbf8168b3
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-06-29 19:41:32 +0300

    Add files via upload

---
+++ NetflixMirror/build.gradle.kts
@@ -0,0 +1,31 @@
+// use an integer for version numbers
+version = 1
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Netflix, PrimeVideo, Disney+"
+    authors = listOf("kerimmkirac")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+
+    iconUrl = "https://netfree2.cc/mobile/img/nf2/icon_x192.png"
+}
