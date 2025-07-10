3ea3524ce067bbfc86c977c1a98a44f79acff15e
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-07-10 14:34:34 +0300

    Dizikorea

---
+++ DiziKorea/src/main/kotlin/com/kerimmkirac/DiziKoreaPlugin.kt
@@ -0,0 +1,14 @@
+package com.kerimmkirac
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class DiziKoreaPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DiziKorea())
+        
+		registerExtractorAPI(PlayerKorea())
+    }
+}
