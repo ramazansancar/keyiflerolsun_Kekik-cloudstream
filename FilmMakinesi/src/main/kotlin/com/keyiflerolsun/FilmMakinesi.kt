6a99179043e651762bde4fb9492b16053ccfcca6
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-07-30 19:01:11 +0300

    FilmMakinesi

--- FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesi.kt
+++ FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesi.kt
@@ -142,19 +142,39 @@ class FilmMakinesi : MainAPI() {
         val iframeGet = app.get(iframe, referer = "${mainUrl}/").document
         val scriptAl  = iframeGet.select("script[type=text/javascript]")[1].data().trim()
         val scriptUnpack = getAndUnpack(scriptAl)
-        val dcRegex = Regex("""dc_[a-zA-Z0-9_]+\(\[(.*?)\]\)""", RegexOption.DOT_MATCHES_ALL)
+        val dchelloVar = if (scriptUnpack.contains("dc_hello")) {
+            "var"
+        } else {
+            "yok"
+        }
+        val dcRegex = if (dchelloVar.contains("var")) {
+            Regex(pattern = "dc_hello\\(\"([^\"]*)\"\\)", options = setOf(RegexOption.IGNORE_CASE))
+        } else {
+            Regex("""dc_[a-zA-Z0-9_]+\(\[(.*?)\]\)""", RegexOption.DOT_MATCHES_ALL)
+        }
         val match = dcRegex.find(scriptUnpack)
-        val parts = match?.groupValues[1]
-            ?.split(",")
-            ?.map { it.trim().removeSurrounding("\"") }
-        Log.d("kraptor_$name", "dc parts: $parts")
-        val decodedUrl = dcDecode(parts!!)
-        Log.d("kraptor_$name", "decoded URL: $decodedUrl")
+//        Log.d("kraptor_$name", "match $match")
+
+        val realUrl = if (dchelloVar.contains("var")) {
+            val parts      = match?.groupValues[1].toString()
+            Log.d("kraptor_$name", "parts $parts")
+            val decodedUrl = dcHello(parts)
+            Log.d("kraptor_$name", "decodedUrl $decodedUrl")
+            decodedUrl
+        } else{
+            val parts = match!!.groupValues[1]
+                .split(",")
+                .map { it.trim().removeSurrounding("\"") }
+            Log.d("kraptor_$name", "dc parts: $parts")
+            val decodedUrl = dcDecode(parts)
+            Log.d("kraptor_$name", "decoded URL: $decodedUrl")
+            decodedUrl
+        }
 
         callback.invoke(newExtractorLink(
             source = this.name,
             name = this.name,
-            url = decodedUrl,
+            url = realUrl,
             type = ExtractorLinkType.M3U8,
             {
                 this.referer = "https://closeload.filmmakinesi.de/"
@@ -196,4 +216,21 @@ fun dcDecode(valueParts: List<String>): String {
     }
 
     return unmix.trim()
+}
+
+fun dcHello(encoded: String): String {
+    // İlk Base64 çöz
+    val firstDecoded = base64Decode(encoded)
+    Log.d("kraptor_hdfilmcehennemi", "firstDecoded $firstDecoded")
+    // Ters çevir
+    val reversed = firstDecoded.reversed()
+    Log.d("kraptor_hdfilmcehennemi", "reversed $reversed")
+    // İkinci Base64 çöz
+    val secondDecoded = base64Decode(reversed)
+
+    val gercekLink    = secondDecoded.substringAfter("http")
+    val sonLink       = "http$gercekLink"
+    Log.d("kraptor_hdfilmcehennemi", "sonLink $sonLink")
+    return sonLink.trim()
+
 }
