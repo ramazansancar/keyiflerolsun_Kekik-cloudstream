94a779595bd2a3d5c0874eb6a4de9d8d445d0be3
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-07-31 23:51:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,6 +83,8 @@ subprojects {
         implementation(kotlin("stdlib"))                                              // Kotlin'in temel kütüphanesi
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP kütüphanesi
         implementation("androidx.annotation:annotation:1.7.1")
+        implementation("com.google.code.gson:gson:2.11.0")
+
         implementation("org.jsoup:jsoup:1.18.3")                                      // HTML ayrıştırıcı
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")   // Kotlin için Jackson JSON kütüphanesi
         implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")          // JSON-nesne dönüştürme kütüphanesi
