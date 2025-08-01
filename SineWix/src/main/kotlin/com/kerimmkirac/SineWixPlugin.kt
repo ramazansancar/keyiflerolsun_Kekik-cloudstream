ff37fd84b414359165dd80110373bb0a7fedc64f
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-08-01 14:21:12 +0300

    Sinewix

---
+++ SineWix/src/main/kotlin/com/kerimmkirac/SineWixPlugin.kt
@@ -0,0 +1,14 @@
+
+package com.kerimmkirac
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class SineWixPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(SineWix())
+        registerExtractorAPI(MediaFireExtractor())
+    }
+}
