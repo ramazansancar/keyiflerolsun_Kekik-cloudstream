59d888ac9dee89e907f60d06d6cdd7d99b22bece
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-08-01 17:06:12 +0300

    Update VidMolyExtractor.kt

--- HintDizi/src/main/kotlin/com/kerimmkirac/VidMolyExtractor.kt
+++ HintDizi/src/main/kotlin/com/kerimmkirac/VidMolyExtractor.kt
@@ -1,6 +1,5 @@
 package com.kerimmkirac
 
-
 import android.util.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
@@ -10,28 +9,41 @@ import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.newExtractorLink
 
+
+class VidMolyTo : VidMolyExtractor() {
+    override var name    = "VidMoly"
+    override var mainUrl = "https://vidmoly.to"
+}
+
 open class VidMolyExtractor : ExtractorApi() {
     override val name            = "VidMoly"
     override val mainUrl         = "https://vidmoly.net"
     override val requiresReferer = true
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
+
+        val url = if (url.contains("https://vidmoly.to")){
+            url.replace("vidmoly.to","vidmoly.net")
+        }else{
+            url
+        }
+
         val headers = mapOf(
             "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
             "Sec-Fetch-Dest" to "iframe",
-            "Referer" to "https://vidmoly.net/"
+            "Referer" to "${mainUrl}/"
         )
-        Log.d("kerimmkirac_$name", "Vidmoly URL'si işleniyor: $url")
+        Log.d("kraptor_$name", "Vidmoly URL'si işleniyor: $url")
         val iSource = app.get(url, headers = headers, referer = "$mainUrl/").text
-        Log.d("kerimmkirac_$name", "Vidmoly iframe içeriği alındı, m3u8 aranıyor...")
+        Log.d("kraptor_$name", "Vidmoly iframe içeriği alındı, m3u8 aranıyor...")
         val matches = Regex("""file\s*:\s*"([^"]+\.m3u8[^"]*)"""").findAll(iSource).toList()
         if (matches.isEmpty()) {
-            Log.w("kerimmkirac_$name", "Vidmoly'de m3u8 link bulunamadı")
+            Log.w("kraptor_$name", "Vidmoly'de m3u8 link bulunamadı")
             return
         }
-        Log.d("kerimmkirac_$name", "Vidmoly'de ${matches.size} adet m3u8 bulundu")
+        Log.d("kraptor_$name", "Vidmoly'de ${matches.size} adet m3u8 bulundu")
         matches.forEachIndexed { index, match ->
             val m3uLink = match.groupValues[1]
-            Log.d("kerimmkirac_$name", "Vidmoly m3uLink[$index] → $m3uLink")
+            Log.d("kraptor_$name", "Vidmoly m3uLink[$index] → $m3uLink")
             callback(
                 newExtractorLink(
                     source = "VidMoly",
@@ -39,7 +51,7 @@ open class VidMolyExtractor : ExtractorApi() {
                     url = m3uLink,
                     type = ExtractorLinkType.M3U8
                 ) {
-                    this.referer = "https://vidmoly.net/"
+                    this.referer = "https://vidmoly.to/"
                     this.quality = Qualities.Unknown.value
                     this.headers = mapOf(
                         "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36"
