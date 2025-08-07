f682f48afdc87c1f3a0d876f8bf15c90a52666f5
keyiflerolsun <keyiflerolsun@users.noreply.github.com>
2025-08-07 18:49:02 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- DiziMom/src/main/kotlin/com/keyiflerolsun/DiziMom.kt
+++ DiziMom/src/main/kotlin/com/keyiflerolsun/DiziMom.kt
@@ -13,7 +13,7 @@ import okhttp3.Response
 import org.jsoup.Jsoup
 
 class DiziMom : MainAPI() {
-    override var mainUrl              = "https://www.dizimom.top"
+    override var mainUrl              = "https://www.dizimom.biz"
     override var name                 = "DiziMom"
     override val hasMainPage          = true
     override var lang                 = "tr"
