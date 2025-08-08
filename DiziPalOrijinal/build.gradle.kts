4d65dd584a1d28ca6663de75db767e26394e8214
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-08-08 13:52:48 +0300

    Update build.gradle.kts

--- DiziPalOrijinal/build.gradle.kts
+++ DiziPalOrijinal/build.gradle.kts
@@ -1,9 +1,9 @@
 
-version = 3
+version = 4
 
 
 cloudstream {
-    authors     = listOf("kerimmkirac")
+    authors     = listOf("kerimmkirac","kraptor")
 
     language    = "tr"
     description = "DiziPal Orijinal."
