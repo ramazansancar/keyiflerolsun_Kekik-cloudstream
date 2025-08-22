07077d26de36461f0d058b5ca32e4835cda776d7
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-08-22 10:55:17 +0300

    SelcukFlix

---
+++ SelcukFlix/src/main/kotlin/com/kerimmkirac/SelcukFlixPlugin.kt
@@ -0,0 +1,32 @@
+package com.kerimmkirac
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class SelcukFlixPlugin: BasePlugin() {
+    override fun load() {
+        registerMainAPI(SelcukFlix())
+        registerExtractorAPI(ContentX())
+        registerExtractorAPI(Hotlinger())
+        registerExtractorAPI(RapidVid())
+        registerExtractorAPI(TRsTX())
+        registerExtractorAPI(VidMoxy())
+        registerExtractorAPI(Sobreatsesuyp())
+        registerExtractorAPI(TurboImgz())
+        registerExtractorAPI(TurkeyPlayer())
+        registerExtractorAPI(Hotlinger())
+        registerExtractorAPI(FourCX())
+        registerExtractorAPI(PlayRu())
+        registerExtractorAPI(FourPlayRu())
+        registerExtractorAPI(FourPichive())
+        registerExtractorAPI(Pichive())
+        registerExtractorAPI(FourDplayer())
+        registerExtractorAPI(SNDplayer())
+        registerExtractorAPI(ORGDplayer())
+        registerExtractorAPI(SnHotLinger())
+        registerExtractorAPI(SNDPlayer74())
+        registerExtractorAPI(VidMolyExtractor())
+        registerExtractorAPI(VidMolyTo())
+    }
+}
