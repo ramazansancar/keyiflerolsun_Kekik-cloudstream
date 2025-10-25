1bfef89ac437db69169faaeb2178efa956701f92
kerimmkirac <kerimmkirac@gmail.com>
2025-10-25 10:52:35 +0300

    yeterla

--- Tafdi/src/main/kotlin/com/nikyokki/Tafdi.kt
+++ Tafdi/src/main/kotlin/com/nikyokki/Tafdi.kt
@@ -83,7 +83,7 @@ class Tafdi : MainAPI() {
         var year = document.selectFirst("div.release a")?.text()?.trim()?.toIntOrNull()
         val tags = document.select("div#listelements a").map { it.text() }
         var rating = document.selectFirst("div.imdb")?.text()?.replace("IMDb Puanı:", "")
-            ?.split("/")?.first()?.trim()?.toRatingInt()
+            ?.split("/")?.first()?.trim()
         var actors = document.select("div.actor a").map { it.text() }
         val trailer = document.selectFirst("div.container iframe")?.attr("src")
         val listItems = document.select("div.list-item")
@@ -96,10 +96,11 @@ class Tafdi : MainAPI() {
             }
         }
         document.select("div#listelements div").forEach {
-            if (it.text().contains("IMDb:")) {
-                rating = it.text().trim().split(" ").last().trim()
-            }
-        }
+    if (it.text().contains("IMDb:")) {
+        rating = it.text().split(" ").last().trim()
+    }
+}
+
 
         return newMovieLoadResponse(title, url, TvType.Movie, url) {
             this.posterUrl = poster
