21296de6574f753b7216533174eb6e4b92444c0e
feroxx <frhatfrat@gmail.com>
2026-08-07 00:05:56 +0300

    czgm

--- DiziPalOriginal/src/main/kotlin/com/keyiflerolsun/DiziPalOriginal.kt
+++ DiziPalOriginal/src/main/kotlin/com/keyiflerolsun/DiziPalOriginal.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
 class DiziPalOriginal : MainAPI() {
-    override var mainUrl              = "https://dizipal2106.com"
+    override var mainUrl              = "https://dizipal2108.com"
     override var name                 = "DiziPalOriginal"
     override val hasMainPage          = true
     override var lang                 = "tr"
