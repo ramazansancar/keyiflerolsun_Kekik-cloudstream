441e482edd6b408cdbec445842e4d9024962f936
patr0n <105314623+patr0nq@users.noreply.github.com>
2026-08-28 20:48:17 +0300

    Update DiziPalOriginal.kt

--- DiziPalOriginal/src/main/kotlin/com/keyiflerolsun/DiziPalOriginal.kt
+++ DiziPalOriginal/src/main/kotlin/com/keyiflerolsun/DiziPalOriginal.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
 class DiziPalOriginal : MainAPI() {
-    override var mainUrl              = "https://dizipal2120.com"
+    override var mainUrl              = "https://dizipal2121.com"
     override var name                 = "DiziPalOriginal"
     override val hasMainPage          = true
     override var lang                 = "tr"
