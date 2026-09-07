9df54f86fb76394980d48b11522ba7f2d706312c
patr0n <105314623+patr0nq@users.noreply.github.com>
2026-09-07 22:40:51 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:9.1.1")
-        classpath("com.github.recloudstream.gradle:gradle-gradle:32895aedb6")
+        classpath("com.github.recloudstream.gradle:com.lagradost.cloudstream3.gradle.gradle.plugin:32895aedb6")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
