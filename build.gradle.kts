a1fab3a324394caa232323af930e68d01600e44f
patr0n <105314623+patr0nq@users.noreply.github.com>
2026-09-07 22:33:44 +0300

    Update Recloudstream Gradle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:9.1.1")
-        classpath("com.github.recloudstream.gradle:32895aedb6")
+        classpath("com.github.recloudstream:gradle:32895aedb6")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
