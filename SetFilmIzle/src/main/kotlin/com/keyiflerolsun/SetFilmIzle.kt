b456dfa9da5fecb611d081996e8091ceacb372fc
patr0n <105314623+patr0nq@users.noreply.github.com>
2026-09-21 16:05:06 +0300

    Update SetFilmIzle.kt

--- SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetFilmIzle.kt
+++ SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetFilmIzle.kt
@@ -13,7 +13,7 @@ import org.jsoup.Jsoup
 import okhttp3.*
 
 class SetFilmIzle : MainAPI() {
-    override var mainUrl              = "https://www.setfilmizle.uk"
+    override var mainUrl              = "https://www.setfilmizle.ltd"
     override var name                 = "SetFilmIzle"
     override val hasMainPage          = true
     override var lang                 = "tr"
